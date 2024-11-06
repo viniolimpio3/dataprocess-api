d849bdd924fdd23f2e6d6e0e6f5769d82b79efd7
davidspdn <davidspdn@gmail.com>
2024-11-05 21:19:45 -0300

    alterações models

--- data-process-api/Models/EmpresaCliente.cs
+++ data-process-api/Models/EmpresaCliente.cs
@@ -1,6 +1,54 @@
+using System.Security.Cryptography.X509Certificates;
+
 namespace data_process_api.Models {
 
+
+    
+
     public class EmpresaCliente {
+
+        public EmpresaCliente()
+        {
+            
+        }
+
+        public EmpresaCliente(string cnpj, string endereco, string telefone, string email)
+        {
+
+            try
+            {
+                if (!string.IsNullOrEmpty(telefone) && !string.IsNullOrEmpty(endereco) && !string.IsNullOrEmpty(cnpj))
+                {
+                    this.Telefone == telefone;
+                    this.Email = email;
+                    this.Cnpj = cnpj;
+                    this.Endereco = endereco;
+
+                }
+              
+            }
+            catch (Exception ex) {
+
+                return ex.Message;
+            }
+        }
+        
+        public EmpresaCliente(string cnpj, string endereco, string telefone)
+        {
+            try { 
+                if (!string.IsNullOrEmpty(telefone) && !string.IsNullOrEmpty(endereco) && !string.IsNullOrEmpty(cnpj)) { 
+                    this.Cnpj = cnpj;
+                    this.Endereco = endereco;
+                    this.Telefone = telefone;
+                }
+            }
+            catch(Exception ex)
+            {
+                return $"Dados invalido: {ex}";
+            }
+        }
+
+        
         public int Id { get; set; }
         public string Telefone { get; set; }
         public string Email { get; set; }
