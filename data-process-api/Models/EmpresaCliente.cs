44d3e6cf36e008b5cf9633c85822d5e5d1515525
Vinícius Olímpio <viniolimpio3@gmail.com>
2024-11-10 23:47:35 -0300

    fix - generic controller and empresaCliente fields

--- data-process-api/Models/EmpresaCliente.cs
+++ data-process-api/Models/EmpresaCliente.cs
@@ -8,31 +8,32 @@ namespace data_process_api.Models {
     public class EmpresaCliente {
 
 
-        public EmpresaCliente(string cnpj, string endereco, string telefone, string email) {
-            if (!string.IsNullOrEmpty(telefone) && !string.IsNullOrEmpty(endereco) && !string.IsNullOrEmpty(cnpj)) {
-                this.Telefone = telefone;
-                this.Email = email;
-                this.Cnpj = cnpj;
-                this.Endereco = endereco;
-            } else {
-                throw new ArgumentException("Campos obrigatórios não preenchidos!");
-            }
-        }
+        //public EmpresaCliente(string cnpj, string endereco, string telefone, string email) {
+        //    if (!string.IsNullOrEmpty(telefone) && !string.IsNullOrEmpty(endereco) && !string.IsNullOrEmpty(cnpj)) {
+        //        this.Telefones = telefone;
+        //        this.Email = email;
+        //        this.Cnpj = cnpj;
+        //        this.Endereco = endereco;
+        //    } else {
+        //        throw new ArgumentException("Campos obrigatórios não preenchidos!");
+        //    }
+        //}
 
-        public EmpresaCliente(string cnpj, string endereco, string telefone) {
-            if (!string.IsNullOrEmpty(telefone) && !string.IsNullOrEmpty(endereco) && !string.IsNullOrEmpty(cnpj)) {
-                this.Cnpj = cnpj;
-                this.Endereco = endereco;
-                this.Telefone = telefone;
-            } else {
-                throw new ArgumentException("Campos obrigatórios não preenchidos!");
-            }
-        }
+        //public EmpresaCliente(string cnpj, string endereco, string telefone) {
+        //    if (!string.IsNullOrEmpty(telefone) && !string.IsNullOrEmpty(endereco) && !string.IsNullOrEmpty(cnpj)) {
+        //        this.Cnpj = cnpj;
+        //        this.Endereco = endereco;
+        //        this.Telefones = telefone;
+        //    } else {
+        //        throw new ArgumentException("Campos obrigatórios não preenchidos!");
+        //    }
+        //}
 
         public int Id { get; set; }
-        public string Telefone { get; set; }
+        public string Telefones { get; set; }
+        public string Nome { get; set; }
         public string? Email { get; set; }
-        public string Cnpj { get; set; }
+        public string? Cnpj { get; set; }
         public string Endereco { get; set; }
         
     }
