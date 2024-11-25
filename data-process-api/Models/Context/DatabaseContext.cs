1719b9c6a867281f553acdebfb4105509ea6c142
Vinícius Olímpio <viniolimpio3@gmail.com>
2024-11-25 20:55:01 -0300

    fix table name

--- data-process-api/Models/Context/DatabaseContext.cs
+++ data-process-api/Models/Context/DatabaseContext.cs
@@ -25,61 +25,61 @@ namespace data_process_api.Models.Context {
         protected override void OnModelCreating(ModelBuilder modelBuilder) {
             base.OnModelCreating(modelBuilder);
 
-            modelBuilder.Entity<Funcionario>().ToTable("Funcionarios");
+            modelBuilder.Entity<Funcionario>().ToTable("funcionarios");
             modelBuilder.Entity<Funcionario>().HasKey(p => p.Id);
 
-            modelBuilder.Entity<Carro>().ToTable("Carros");
+            modelBuilder.Entity<Carro>().ToTable("carros");
             modelBuilder.Entity<Carro>().HasKey(p => p.Id);
 
-            modelBuilder.Entity<Usuario>().ToTable("Usuarios");
+            modelBuilder.Entity<Usuario>().ToTable("usuarios");
             modelBuilder.Entity<Usuario>().HasKey(p => p.Id);
 
-            modelBuilder.Entity<Abastecimento>().ToTable("Abastecimentos");
+            modelBuilder.Entity<Abastecimento>().ToTable("abastecimentos");
             modelBuilder.Entity<Abastecimento>().HasKey(p => p.Id);
 
-            modelBuilder.Entity<CarroFuncionario>().ToTable("CarrosFuncioanarios");
+            modelBuilder.Entity<CarroFuncionario>().ToTable("carrosfuncioanarios");
             modelBuilder.Entity<CarroFuncionario>().HasKey(p => p.Id);
 
-            modelBuilder.Entity<EmpresaCliente>().ToTable("EmpresasCliente");
+            modelBuilder.Entity<EmpresaCliente>().ToTable("empresascliente");
             modelBuilder.Entity<EmpresaCliente>().HasKey(p => p.Id);
 
-            modelBuilder.Entity<Entrada>().ToTable("Entradas");
+            modelBuilder.Entity<Entrada>().ToTable("entradas");
             modelBuilder.Entity<Entrada>().HasKey(p => p.Id);
 
-            modelBuilder.Entity<Fornecedor>().ToTable("Fornecedores");
+            modelBuilder.Entity<Fornecedor>().ToTable("fornecedores");
             modelBuilder.Entity<Fornecedor>().HasKey(p => p.Id);
 
-            modelBuilder.Entity<Frete>().ToTable("Fretes");
+            modelBuilder.Entity<Frete>().ToTable("fretes");
             modelBuilder.Entity<Frete>().HasKey(p => p.Id);
 
-            modelBuilder.Entity<RepasseMotorista>().ToTable("RepasseMotorista");
+            modelBuilder.Entity<RepasseMotorista>().ToTable("repassemotorista");
             modelBuilder.Entity<RepasseMotorista>().HasKey(p => p.Id);
 
-            modelBuilder.Entity<FormaPagamento>().ToTable("FormasPagamento");
+            modelBuilder.Entity<FormaPagamento>().ToTable("formaspagamento");
             modelBuilder.Entity<FormaPagamento>().HasKey(p => p.Id);
 
-            modelBuilder.Entity<Manutencao>().ToTable("Manutencoes");
+            modelBuilder.Entity<Manutencao>().ToTable("manutencoes");
             modelBuilder.Entity<Manutencao>().HasKey(p => p.Id);
 
-            modelBuilder.Entity<PrecoPadrao>().ToTable("PrecosPadrao");
+            modelBuilder.Entity<PrecoPadrao>().ToTable("precospadrao");
             modelBuilder.Entity<PrecoPadrao>().HasKey(p => p.Id);
 
-            modelBuilder.Entity<Regiao>().ToTable("Regiao");
+            modelBuilder.Entity<Regiao>().ToTable("regiao");
             modelBuilder.Entity<Regiao>().HasKey(p => p.Id);
 
-            modelBuilder.Entity<Saida>().ToTable("Saidas");
+            modelBuilder.Entity<Saida>().ToTable("saidas");
             modelBuilder.Entity<Saida>().HasKey(p => p.Id);
 
-            modelBuilder.Entity<TipoEntrada>().ToTable("TiposEntrada");
+            modelBuilder.Entity<TipoEntrada>().ToTable("tiposentrada");
             modelBuilder.Entity<TipoEntrada>().HasKey(p => p.Id);
 
-            modelBuilder.Entity<TipoFuncionario>().ToTable("TiposFuncionario");
+            modelBuilder.Entity<TipoFuncionario>().ToTable("tiposfuncionario");
             modelBuilder.Entity<TipoFuncionario>().HasKey(p => p.Id);
 
-            modelBuilder.Entity<TipoSaida>().ToTable("TiposSaida");
+            modelBuilder.Entity<TipoSaida>().ToTable("tipossaida");
             modelBuilder.Entity<TipoSaida>().HasKey(p => p.Id);
 
-            modelBuilder.Entity<Usuario>().ToTable("Usuarios");
+            modelBuilder.Entity<Usuario>().ToTable("usuarios");
             modelBuilder.Entity<Usuario>().HasKey(p => p.Id);
 
 
